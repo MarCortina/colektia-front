51b7e1742dfb095268df5fdd4b263f7c74f4e90c
MarCortina <marmica24@hotmail.com>
2021-06-06 21:43:56 -0300

    front

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  
+      <Movies msg="Lista de peliculas recomendadas" />
+    
+</template>
+
+<script>
+import Movies from "./components/Movies.vue";
+
+export default {
+  name: "App",
+  components: {
+    Movies,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Movies.vue
@@ -0,0 +1,164 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div>
+    <nav class="navbar navbar-light bg-light bg-dark">
+      <a href="/" class="navbar-brand text-white"> Refresh </a>
+    </nav>
+
+    <div class="container">
+      <div class="row pt-5">
+        <div class="col-md-5">
+          <div class="card">
+            <div class="card-body">
+              <form @submit.prevent="addMovie">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    v-model="movie.title"
+                    placeholder="Title"
+                    class="form-control"
+                  />
+                </div>
+
+                <div class="form-group">
+                  <textarea
+                    v-model="movie.description"
+                    cols="30"
+                    rows="10"
+                    class="form-control"
+                    placeholder="Description"
+                  ></textarea>
+                </div>
+                <template v-if="edit === false">
+                  <button class="btn btn-primary btn-block">Send</button>
+                </template>
+                <template v-else>
+                  <button class="btn btn-primary btn-block">Update</button>
+                </template>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-7">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>MOVIE</th>
+                <th>DESCRIPTION</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="movie of movies" :key="movie">
+                <td>{{ movie.title }}</td>
+                <td>{{ movie.description }}</td>
+                <td>
+                  <button @click="delete movie._id" class="btn btn-danger">
+                    DELETE
+                  </button>
+                  <button @click="editMovie(movie._id)" class="btn-secondary">
+                    EDIT
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+class Movie {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+  }
+}
+
+export default {
+  data() {
+    return {
+      movie: new Movie(),
+      movies: [],
+      edit: false,
+      toEdit: "",
+    };
+  },
+  created() {
+    this.getMovie();
+  },
+  name: "Movies",
+  props: {
+    msg: String,
+  },
+  methods: {
+    getMovie() {
+      fetch("/")
+        .then((res) => res.json())
+        .then((data) => {
+          this.movies = data;
+        });
+    },
+    addMovie() {
+      if (this.edit === false) {
+        fetch("/api", {
+          method: "POST",
+          body: JSON.stringify(this.movie),
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            this.getMovie(data);
+          });
+      } else {
+        fetch("/api" + this.toEdit, {
+          method: "PUT",
+          body: JSON.stringify(this.movie),
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            this.getMovie(data);
+            this.edit = false;
+          });
+      }
+
+      this.movie = new Movie();
+    },
+    delete(id) {
+      fetch("/api" + id, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          this.getMovie(data);
+        });
+    },
+    editMovie(id) {
+      fetch("/api" + id)
+        .then((res) => res.json())
+        .then((data) => {
+          this.movie = new Movie(data.title, data.description);
+          this.toEdit = data._id;
+          this.edit = true;
+        });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
